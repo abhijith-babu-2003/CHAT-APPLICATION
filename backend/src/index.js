682787f2a46f170df379ce91ef06d0a8f6a7a6e0
git practice <abhijith7133@gmail.com>
2025-07-16 18:21:57 +0530

    Fix __dirname for Render and deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cookieParser())
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 10000;
 
 app.use(
   cors({
@@ -34,7 +34,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
   })
